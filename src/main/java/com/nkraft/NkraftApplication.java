84eeb5174f1a6717d3abff78e5ddc7776df1c206
natsuki <n.kaido@ichirirop.com>
2025-07-11 02:26:44 +0900

    初回スケルトン

---
+++ src/main/java/com/nkraft/NkraftApplication.java
@@ -0,0 +1,13 @@
+package com.nkraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nkraft/NkraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
